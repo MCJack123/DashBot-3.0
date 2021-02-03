a5c5ba391539112f00972f81513f6b8172f29b54
JackMacWindows <jackmacwindowslinux@gmail.com>
2021-02-02 23:55:30 -0500

    HUGE changes (still beta)
    * DashBot can now run in the background and no longer uses the mouse
    * Added in a few ways to attempt fixes when things break
    * Portals are now auto-detected, so portal info is no longer required or wanted
    * Adjusted how deaths are detected
    
    This is still a bit messy, and things may break, but it's much improved now.

--- dashbot.cpp
+++ dashbot.cpp
@@ -11,37 +11,77 @@
 #include <string>
 #include <cstring>
 #include <algorithm>
+#include <list>
 #undef max
 
 double MAX_ODDS = 4.0;
+int FPS = 360;
 #define ODDS ((bestLastJump == 0.0 ? (bestFitness / 2.0) : bestLastJump) / bestFitness) / (100.0 / MAX_ODDS) + 0.0001
 
-const std::unordered_map<std::string, std::vector<bool> > flyStatesForAllMaps = {
-    {"StereoMadness", {false, true, false, true}},
-    {"BackOnTrack", {false, true, false}},
-    {"Polargeist", {false, true, false}},
-    {"DryOut", {false, true, false}},
-    {"BaseAfterBase", {false, true, false}},
-    {"CantLetGo", {false, true, false}},
-    {"Jumper", {false, true, false, true, false}},
-    {"TimeMachine", {false, true, false, true}},
-    {"Cycles", {false, false /*Ball*/, true, false, false}},
-    {"xStep", {false, false, true, false, true, false, false, true}},
-    {"Clutterfunk", {false, true, false, true, false}},
-    {"TheoryOfEverything", {false, true, false, false /*UFO*/, false, false, true, false, false, false, false, true}},
-    {"ElectromanAdventures", {false, true, false, false, false, false, true, false, true, false, false, false}},
-    {"Clubstep", {false, true, false, false, false, true, false, true, false, true, false, true, false, true}},
-    {"Electrodynamix", {false, true, false, false, false, false, true, false, true, false, false, true}},
-    {"HexagonForce", {false, true, false, false, true, false, false, false, true, false, true}},
-    {"BlastProcessing", {false, true /*Wave*/, true, false, true, false, false, true, false, false, true, false}},
-    {"TheoryOfEverything2", {false, true, false, false, false, false, true, false, true, false, true, true, false, true, false, false, true, false}},
-    {"GeometricalDominator", {false, true /*Robot*/, true, true, false, true, true, false, false, true, false, false, true, true}},
-    //{"Deadlocked", {}},
-    // Fingerdash is currently impossible (?) due to the hold jump rings, though it may work to have the cube part be mode 1 instead
+/*
+ * We can now read the mode. as well as some other stuff, with the offset GeometryDash.exe + 3222D0 + 164 + 224 + 638.
+ * This is an "array" of booleans with the contents:
+ * [0]: Is ship?
+ * [1]: Is UFO? (interesting that it's earlier than ball, since UFO is newer)
+ * [2]: Is ball?
+ * [3]: Is wave?
+ * [4]: Is robot?
+ * [5]: Is spider?
+ * [6]: Is upside-down?
+ * [7]: Is dead?
+ */
+
+enum class Mode {
+    cube,
+    ship,
+    ball,
+    ufo,
+    wave,
+    robot,
+    spider,
+    unknown
 };
 
+const char * modeNames[] = {
+    "cube",
+    "ship",
+    "ball",
+    "ufo",
+    "wave",
+    "robot",
+    "spider",
+    "unknown"
+};
+
+bool modeFlying[] = {
+    false,
+    true,
+    false,
+    false,
+    true,
+    true,
+    false,
+    false
+};
+
+bool isDead = false;
+Mode toMode(uint32_t hi, uint32_t lo) {
+    isDead = hi & 0x1000000;
+    hi &= 0xFFFF;
+    if (hi == 0) {
+        if (lo == 0) return Mode::cube;
+        else if (lo == 0x1) return Mode::ship;
+        else if (lo == 0x100) return Mode::ufo;
+        else if (lo == 0x10000) return Mode::ball;
+        else if (lo == 0x1000000) return Mode::wave;
+        else return Mode::unknown;
+    } else if (hi == 0x1) return Mode::robot;
+    else if (hi == 0x100) return Mode::spider;
+    else return Mode::unknown;
+}
+
 const std::unordered_map<std::string, double> oddsForAllMaps = {
-    {"StereoMadness", 4.0},
+    {"StereoMadness", 3.0},
     {"BackOnTrack", 4.0},
     {"Polargeist", 4.0},
     {"DryOut", 4.0},
@@ -64,7 +104,6 @@ const std::unordered_map<std::string, double> oddsForAllMaps = {
     {"Fingerdash", 5.0}
 };
 
-std::vector<bool> flyStatesForMap;
 double bestFitness = 1;
 double bestFitnessR = 0;
 std::set<uint32_t> bestJumps;
@@ -73,9 +112,8 @@ int failCount = 0;
 float lastX = 0;
 std::ofstream out;
 bool saving = false;
-uint32_t oldFlyState = 0;
-int currentFlyNum = 0;
 bool isDown = false;
+Mode mode = Mode::cube;
 
 void saveLevel(const char * argv[]) {
     if (!saving) return;
@@ -94,36 +132,26 @@ void saveLevel(const char * argv[]) {
     } while (loop);
 }
 
-inline int getlen(const char * str) {
-    const char * pos = strchr(str, ':');
-    if (pos) return pos - str;
-    else return strlen(str);
-}
-
 int main(int argc, const char * argv[]) {
-    if (argc < 2) {
-        std::cerr << "Usage: " << argv[0] << " <level name|portal types> [save.dbj]\nType \"" << argv[0] << " list\" to list level names\n";
-        return 1;
-    } else if (std::string(argv[1]) == "--help" || std::string(argv[1]) == "-h" || std::string(argv[1]) == "-?") {
-        std::cout << "DashBot is a simple AI designed to beat Geometry Dash levels. This version works with Geometry Dash 2.11.\nUsage: " << argv[0] << " <level name|portal types> [save.dbj]\nType \"" << argv[0] << " list\" to list level names.\n";
-        return 0;
-    } else if (std::string(argv[1]) == "list") {
-        std::cout << "List of known level names: ";
-        for (std::pair<std::string, std::vector<bool> > p : flyStatesForAllMaps) std::cout << p.first << ", ";
-        std::cout << "\nTo use DashBot with another level, you must supply the portal type list yourself. The string must be a binary string specifying whether the bot should tap (0: cube, ball, UFO, spider) or hold (1: ship, wave, robot?) the mouse. Each digit represents a region between two of either the start of the map, the end of the map, or a portal. For example, Stereo Madness's portal string would be '0101' since it's mode sequence is cube, ship, cube, ship.\n";
-        return 0;
-    } else if (flyStatesForAllMaps.find(argv[1]) != flyStatesForAllMaps.end()) {
-        flyStatesForMap = flyStatesForAllMaps.at(argv[1]);
-        MAX_ODDS = oddsForAllMaps.at(argv[1]);
-    } else if (std::all_of(argv[1], argv[1] + getlen(argv[1]), [](char c)->bool{return c == '0' || c == '1';})) {
-        for (int i = 0; argv[1][i] && argv[1][i] != ':'; i++) flyStatesForMap.push_back(argv[1][i] == '1');
-        if (strchr(argv[1], ':')) MAX_ODDS = atof(strchr(argv[1], ':') + 1);
+    if (argc > 1) {
+        if (std::string(argv[1]) == "--help" || std::string(argv[1]) == "-h" || std::string(argv[1]) == "-?") {
+            std::cout << "DashBot is a simple AI designed to beat Geometry Dash levels. This version works with Geometry Dash 2.11.\nUsage: " << argv[0] << " [level name|jump speed] [save.dbj]\nType \"" << argv[0] << " list\" to list level names.\n";
+            return 0;
+        } else if (std::string(argv[1]) == "list") {
+            std::cout << "List of known level names: ";
+            for (std::pair<std::string, double> p : oddsForAllMaps) std::cout << p.first << ", ";
+            std::cout << "\nTo use DashBot with another level, you can supply the jump speed (defaults to 4.0). A higher value will cause DashBot to jump more often. Use a higher value for faster/harder levels, and a lower value for slower levels.\n";
+            return 0;
+        } else if (oddsForAllMaps.find(argv[1]) != oddsForAllMaps.end()) {
+            MAX_ODDS = oddsForAllMaps.at(argv[1]) * (60.0 / FPS);
+        } else if (std::all_of(argv[1], argv[1] + strlen(argv[1]), [](char c)->bool{return isdigit(c) || c == '.';})) {
+            MAX_ODDS = atof(argv[1]);
+        } else {
+            std::cerr << "Usage: " << argv[0] << " [level name|jump speed] [save.dbj]\nType \"" << argv[0] << " list\" to list level names\n";
+            return 1;
+        }
     }
-    else {
-        std::cerr << "Usage: " << argv[0] << " <level name|portal types> [save.dbj]\nType \"" << argv[0] << " list\" to list level names\n";
-        return 1;
-    }
-    std::cout << "DashBot v3.0\nBased on Pizzabot-v4 by Pizzaroot\n";
+    std::cout << "DashBot v3.1\nBased on Pizzabot-v4 by Pizzaroot\n";
     if (argc > 2) {
         saving = true;
         // file format:
@@ -152,14 +180,40 @@ int main(int argc, const char * argv[]) {
     std::set<uint32_t> jumps;
     double lastJump = 0;
     bool randing = false;
-    oldFlyState = GD.Read<uint32_t>({ GD.BaseAddress , 0x3222D0 , 0x164, 0x224, 0x534 });
+    std::list<uint32_t> lastDeathPositions;
+    float globalOffset = 0.0;
+    float offsetDelta = 2.0;
+    float lastY = 0.0;
+    /* === desync debugging === //
+    float globalDesync = 0.0;
+    RegisterHotKey(NULL, 1, MOD_CONTROL, VK_ADD);
+    RegisterHotKey(NULL, 2, MOD_CONTROL, VK_SUBTRACT);
+    // ======================== */
     while (true) {
+        /* === desync debugging === //
+        MSG message;
+        if (PeekMessage(&message, NULL, WM_HOTKEY, WM_HOTKEY, PM_REMOVE)) {
+            if (message.wParam == 1) {globalDesync += 1.0; std::cout << "simulating desync by +1.0 (now " << globalDesync << ")\n";}
+            else if (message.wParam == 2) {globalDesync -= 1.0; std::cout << "simulating desync by -1.0 (now " << globalDesync << ")\n";}
+        }
+        // ======================== */
         bool shouldJump = false;
-        float xPos = GD.Read<float>({ GD.BaseAddress , 0x3222D0 , 0x164, 0x224, 0x67C });
-        float yPos = GD.Read<float>({ GD.BaseAddress , 0x3222D0 , 0x164, 0x38C, 0xB4, 0x224, 0x680 });
-        uint32_t flyState = xPos != lastX ? GD.Read<uint32_t>({ GD.BaseAddress , 0x3222D0 , 0x164 , 0x224 , 0x534 }) : oldFlyState;
-        char percentage0 = GD.Read<char>({ GD.BaseAddress , 0x3222D0 , 0x164, 0x404, 0xB4, 0x3C0, 0xE8, 0x8, 0x12C });
-        if ((percentage0 == '1' && GD.Read<char>({ GD.BaseAddress , 0x3222D0 , 0x164, 0x404, 0xB4, 0x3C0, 0xE8, 0x8, 0x12D }) == '0' && GD.Read<char>({ GD.BaseAddress , 0x3222D0 , 0x164, 0x404, 0xB4, 0x3C0, 0xE8, 0x8, 0x12E }) == '0')) {
+        float xPos = GD.Read<float>({ GD.BaseAddress , 0x3222D0 , 0x164, 0x224, 0x67C }) + globalOffset /*+ globalDesync*/; // === desync debugging ===
+        float yPos = GD.Read<float>({ GD.BaseAddress , 0x3222D0 , 0x164, 0x224, 0x680 });
+        if (xPos != lastX) {
+            Mode oldmode = mode;
+            mode = toMode(GD.Read<uint32_t>({GD.BaseAddress, 0x3222D0, 0x164, 0x224, 0x63C}), GD.Read<uint32_t>({GD.BaseAddress, 0x3222D0, 0x164, 0x224, 0x638}));
+            if (mode != oldmode && xPos > lastX) {
+                std::cout << "switched between modes (" << modeNames[(int)oldmode] << " -> " << modeNames[(int)mode] << ")\n";
+                if (isDown) {
+                    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                    isDown = false;
+                }
+            }
+        }
+        uint32_t percentage_int = GD.Read<uint32_t>({ GD.BaseAddress , 0x3222D0 , 0x164, 0x3C0, 0xE8, 0x8, 0x12C });
+        char percentage[4] = {percentage_int & 0xFF, (percentage_int >> 8) & 0xFF, (percentage_int >> 16) & 0xFF, (percentage_int >> 24) & 0xFF};
+        if (percentage[0] == '1' && percentage[1] == '0' && percentage[2] == '0' && percentage[3] == '%') {
             std::cout << "level complete! saving and exiting\n";
             bestFitness = lastX;
             bestJumps = jumps;
@@ -167,35 +221,30 @@ int main(int argc, const char * argv[]) {
             saveLevel(argv);
             return 0;
         }
-        if (flyState != oldFlyState && xPos > lastX) {
-            std::cout << "switched between modes (" << flyStatesForMap[currentFlyNum] << " -> " << flyStatesForMap[currentFlyNum+1] << ")\n";
-            currentFlyNum++;
-            oldFlyState = flyState;
-            if (isDown) {
-                mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
-                isDown = false;
-            }
-        }
-        if (xPos < lastX && lastX - xPos > 10) {
-            std::cout << "dead (" << lastX << ") (";
+        if (xPos > lastX && abs(lastX - globalOffset) < 0.5) std::cout << "[research] delta at beginning is " << xPos - lastX << ", offset is " << xPos - globalOffset - 5.0 << "\n";
+        if (isDead) {
+            std::cout << "dead (" << lastX << ", " << lastY << ") (";
             randing = false;
-            oldFlyState = flyState;
-            currentFlyNum = 0;
+            lastDeathPositions.push_back((uint32_t)lastX);
+            if (lastDeathPositions.size() > 5) lastDeathPositions.pop_front();
             if (isDown) {
-                mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                GD.Write<bool>({GD.BaseAddress, 0x3222D0, 0x164, 0x224, 0x611}, false);
+                GD.Write<bool>({GD.BaseAddress, 0x3222D0, 0x164, 0x224, 0x612}, false);
                 isDown = false;
             }
-            if (lastX > bestFitness && (lastX > bestFitnessR || failCount < 9)) {
-                std::cout << (10 - failCount - (lastX <= bestFitnessR)) << " tries until regression)\n" << "best fitness @ " << lastX << " vs. " << bestFitness << "\n";
+            if (lastX > bestFitness && (lastX > bestFitnessR || failCount < 9) && lastY < 2750.0) {
+                std::cout << (10 - failCount /*- (lastX <= bestFitnessR)*/) << " tries until regression)\n" << "best fitness @ " << lastX << " vs. " << bestFitness << "\n";
                 bestFitness = lastX;
                 if (lastX > bestFitnessR) {
                     bestJumps = jumps;
                     bestLastJump = lastJump;
                     bestFitnessR = 0;
-                    saveLevel(argv);
                     failCount = 0;
+                    //globalOffset = 0.0;
+                    offsetDelta = 2.0;
+                    saveLevel(argv);
                 } else {
-                    failCount++;
+                    //failCount++;
                     std::cout << "not saving this one since it can be better\n";
                 }
             } else if ((signed)bestJumps.size() - (signed)jumps.size() <= 10 && ++failCount >= 10 && bestJumps.size() > 0) {
@@ -212,13 +261,56 @@ int main(int argc, const char * argv[]) {
                 bestFitnessR = bestFitness;
                 bestFitness = bestLastJump;
                 failCount = 0;
+                //globalOffset = 0.0;
+                offsetDelta = 2.0;
                 saveLevel(argv);
+            } else if (lastDeathPositions.size() >= 5 && (/*bestFitness < 1000.0 ||*/ lastX < bestFitness - 1000.0) && std::all_of(lastDeathPositions.begin(), lastDeathPositions.end(), [lastDeathPositions](uint32_t val) -> bool {return abs((double)val - (double)lastDeathPositions.front()) < 15.0;})) { // the last 5 deaths were within 15 units of each other
+                std::cout << (10 - failCount) << " tries until regression)\n" << "we're stuck here! ";
+                std::list<uint32_t> lostJumps;
+                for (int i = 0; i < 200; i += 5) {
+                    if (bestJumps.find((uint32_t)floor(lastX / 5) * 5 - i) != bestJumps.end() && jumps.find((uint32_t)floor(lastX / 5) * 5 - i) == jumps.end()) {
+                        lostJumps.push_back((uint32_t)floor(lastX / 5) * 5 - i);
+                    }
+                }
+                // try to fix any lost jumps
+                if (!lostJumps.empty()) {
+                    std::cout << "looks like some jumps were lost - fixing\n";
+                    // for now we'll just subtract 5 from each lost jump
+                    // maybe we'll need to check to see if it needs to go up/down?
+                    for (uint32_t jump : lostJumps) {
+                        std::cout << "fixing lost jump at " << jump << "\n";
+                        bestJumps.erase(jump);
+                        bestJumps.insert(jump - 5);
+                    }
+                // adjust offsets if the coordinates desynced
+                } else {
+                    std::cout << "looks like the coordinates got desynced - fixing\n";
+                    if (globalOffset >= offsetDelta * 8.0) {
+                        if (offsetDelta <= 0.25) {
+                            std::cout << "looks like it's broken for good - trying again, but manual intervention will likely be necessary\n";
+                            globalOffset = 0.0;
+                            offsetDelta = 2.0;
+                        } else {
+                            globalOffset = 0.0;
+                            offsetDelta /= 2.0;
+                            std::cout << "offset went over the offset boundaries, restarting with new delta " << offsetDelta << "\n";
+                        }
+                    } else if (globalOffset > 0.0) globalOffset = -globalOffset;
+                    else globalOffset = -globalOffset + offsetDelta;
+                    std::cout << "global offset is now at " << globalOffset << "\n";
+                    // don't try to adjust for the next 10 attempts
+                    lastDeathPositions.push_back(0);
+                    lastDeathPositions.pop_front();
+                }
             } else {
                 std::cout << (10 - failCount) << " tries until regression)\n";
+		        if (lastX > bestFitness && (lastX > bestFitnessR || failCount < 9) && lastY >= 2750.0) std::cout << "not saving this one since we fell out of bounds\n";
                 //bestFitnessR = 0;
             }
             jumps.clear();
             lastJump = 0;
+            while (isDead) mode = toMode(GD.Read<uint32_t>({GD.BaseAddress, 0x3222D0, 0x164, 0x224, 0x63C}), GD.Read<uint32_t>({GD.BaseAddress, 0x3222D0, 0x164, 0x224, 0x638}));
+            continue;
         } else if ((uint32_t)floor(xPos / 5) * 5 <= bestLastJump) {
             if (bestJumps.find((uint32_t)floor(xPos / 5) * 5) != bestJumps.end() && jumps.find((uint32_t)floor(xPos / 5) * 5) == jumps.end())
                 shouldJump = true;
@@ -231,18 +323,22 @@ int main(int argc, const char * argv[]) {
             if (rnum < ODDS) shouldJump = true;
         }
         if (shouldJump) {
-            std::cout << "clicking (" << (uint32_t)floor(xPos / 5) * 5 << ")\n";
+            std::cout << "clicking" << (modeFlying[(int)mode] ? (isDown ? " off " : " on  ") : " ") << "(" << (uint32_t)floor(xPos / 5) * 5 << ")\n";
             jumps.insert((uint32_t)floor(xPos / 5) * 5);
             lastJump = floor(xPos / 5) * 5;
-            if (flyStatesForMap[currentFlyNum]) {
-                mouse_event(isDown ? MOUSEEVENTF_LEFTUP : MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+            if (modeFlying[(int)mode]) {
                 isDown = !isDown;
+                GD.Write<bool>({GD.BaseAddress, 0x3222D0, 0x164, 0x224, 0x611}, isDown);
+                GD.Write<bool>({GD.BaseAddress, 0x3222D0, 0x164, 0x224, 0x612}, isDown);
             } else {
-                mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                GD.Write<bool>({GD.BaseAddress, 0x3222D0, 0x164, 0x224, 0x611}, true);
+                GD.Write<bool>({GD.BaseAddress, 0x3222D0, 0x164, 0x224, 0x612}, true);
                 Sleep(75);
-                mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                GD.Write<bool>({GD.BaseAddress, 0x3222D0, 0x164, 0x224, 0x611}, false);
+                GD.Write<bool>({GD.BaseAddress, 0x3222D0, 0x164, 0x224, 0x612}, false);
             }
         }
+        if (xPos > lastX) lastY = yPos;
         lastX = xPos;
     }
 }
